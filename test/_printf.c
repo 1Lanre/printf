e08600ef7cb47926da6c85349f0cd787f7aa29d0
1Lanre <ogunbiyilanre205@gmail.com>
2024-07-27 14:32:44 -0700

    uploading test files

--- test/_printf.c
+++ test/_printf.c
@@ -1,92 +1,68 @@
-#include "main.h"
 #include <stdarg.h>
-#include <unistd.h>
-
-/* Function prototypes */
-static void flush_buffer(char buffer[], int *buff_ind);
-static int process_format(const char *format, int *i, va_list list, char buffer[], int *buff_ind);
+#include "main.h"
 
 /**
- * _printf - Custom printf function
- * @format: Format string
- * Return: Number of characters printed, or -1 on failure
+ * _printf - Produces output according to a format string.
+ * @format: Format string containing the characters and specifiers.
+ *
+ * Return: Length of the formatted output string.
  */
 int _printf(const char *format, ...)
 {
-    int i = 0, printed_chars = 0;
-    va_list list;
-    char buffer[BUFF_SIZE];
-    int buff_ind = 0;
+    va_list args;
+    int count = 0;
+    int i = 0;
 
     if (!format)
         return (-1);
 
-    va_start(list, format);
+    va_start(args, format);
 
     while (format[i] != '\0')
     {
         if (format[i] != '%')
         {
-            buffer[buff_ind++] = format[i];
-            if (buff_ind >= BUFF_SIZE)
-                flush_buffer(buffer, &buff_ind);
-            printed_chars++;
+            count += _putchar(format[i]);
+            i++;
         }
         else
         {
-            flush_buffer(buffer, &buff_ind);
-            if (process_format(format, &i, list, buffer, &buff_ind) == -1)
-            {
-                va_end(list);
-                return (-1);
-            }
-            printed_chars += buff_ind;
+            i++;
+            count += handle_specifier(format[i], args);
+            i++;
         }
-        i++;
     }
 
-    flush_buffer(buffer, &buff_ind);
-    va_end(list);
-
-    return printed_chars;
+    va_end(args);
+    return (count);
 }
 
 /**
- * flush_buffer - Flushes the buffer to the output
- * @buffer: Array of chars
- * @buff_ind: Index indicating the number of valid characters in buffer
+ * handle_specifier - Processes format specifiers and prints accordingly.
+ * @specifier: The format specifier character.
+ * @args: The va_list containing the arguments.
+ *
+ * Return: Number of characters printed.
  */
-static void flush_buffer(char buffer[], int *buff_ind)
+int handle_specifier(char specifier, va_list args)
 {
-    if (*buff_ind > 0)
+    switch (specifier)
     {
-        write(1, buffer, *buff_ind);
-        *buff_ind = 0;
+        case 'c':
+            return _putchar(va_arg(args, int));
+        case 's':
+            return print_string(va_arg(args, char *));
+        case '%':
+            return _putchar('%');
+        case 'd':
+        case 'i':
+            return print_decimal(va_arg(args, int));
+        case 'b':
+            return print_binary(va_arg(args, int));
+        case 'u':
+            return print_unsigned(va_arg(args, unsigned int));
+        default:
+            return _putchar('?'); // Print '?' for unsupported specifiers
     }
-}
-
-/**
- * process_format - Processes the format specifiers
- * @format: Format string
- * @i: Index of the current character in format
- * @list: Argument list
- * @buffer: Output buffer
- * @buff_ind: Buffer index
- * Return: Number of characters processed, or -1 on error
- */
-static int process_format(const char *format, int *i, va_list list, char buffer[], int *buff_ind)
-{
-    int flags, width, precision, size;
-    int printed;
-
-    flags = get_flags(format, i);
-    width = get_width(format, i, list);
-    precision = get_precision(format, i, list);
-    size = get_size(format, i);
-    (*i)++;
-
-    printed = handle_print(format, i, list, buffer, flags, width, precision, size);
-
-    return printed;
 }
 
